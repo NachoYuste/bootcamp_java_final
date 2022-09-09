ec8442e1d9777a0b90adcd76c7619f667916f6b5
NachoYuste <ignacioyustel@live.com>
2022-09-09 11:02:16 +0200

    Creación de repositorio de Tutorial y tests

--- src/main/java/data/jpa/test/SpringBootDatajpatestApplication.java
+++ src/main/java/data/jpa/test/SpringBootDatajpatestApplication.java
@@ -1,5 +1,12 @@
 package data.jpa.test;
 
-public class SpringBootDatajpatestApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class SpringBootDatajpatestApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDatajpatestApplication.class, args);
+	}
 }
--- src/main/java/data/jpa/test/respository/TutorialRepository.java
+++ src/main/java/data/jpa/test/respository/TutorialRepository.java
@@ -1,5 +1,15 @@
 package data.jpa.test.respository;
 
-public class TutorialRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import data.jpa.test.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long>{
+	
+	List<Tutorial> findByPublished(boolean published);
+	
+	List <Tutorial> findByTitleContaining(String title);
 
 }
